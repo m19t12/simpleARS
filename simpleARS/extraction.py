855789f003eb98364f50234f8847a440432d8e81
m19t12 <emmtsoukalas@gmail.com>
2017-03-22 16:24:06 +0200

    refactor header function in extraction.py and added some tests

--- simpleARS/extraction.py
+++ simpleARS/extraction.py
@@ -1,36 +1,22 @@
 import csv
-import logging
+
+from simpleARS import search_object
 
 
-def create_header(retrieved_data):
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
+def create_header(search_json):
     header = []
+    search = search_object.SearchObject(search_json)
+    select_keys = search.src_select
 
-    for key in retrieved_data:
-        header.append(key)
+    for keys in select_keys:
+        if isinstance(keys, dict):
+            sub_header = create_header(keys)
+            for sub_keys in sub_header:
+                header.append(sub_keys)
+        else:
+            header.append(keys)
 
-    if len(header) > 1:
-        sub_headers = []
-        for key in header:
-            if isinstance(retrieved_data[key], dict):
-                for dict_key in list(retrieved_data[key]):
-                    sub_headers.append(dict_key)
-            elif isinstance(retrieved_data[key], list):
-                logger.info("Relationship one to many limit one activate")
-                list_data = retrieved_data[key][0]
-                list_header = create_header(list_data)
-                for list_keys in list_header:
-                    sub_headers.append(list_keys)
-            else:
-                sub_headers.append(key)
-        return sub_headers
-    else:
-        key = header[0]
-        if isinstance(retrieved_data[key], list):
-            data = retrieved_data[key][0]
-            header = create_header(data)
-        return header
+    return header
 
 
 def find_relationships(save_data):
@@ -68,9 +54,10 @@ def get_save_data(retrieved_data):
                 for data in list_item:
                     dict_object[data] = list_item[data]
             elif isinstance(retrieved_data[key], dict):
-                list_item = get_save_data(retrieved_data[key])
-                for data in list_item:
-                    dict_object[key] = data
+                # list_item = get_save_data(retrieved_data[key])
+                # for data in list_item:
+                #     dict_object[key] = data
+                dict_object[key] = retrieved_data[key]
             else:
                 dict_object[key] = retrieved_data[key]
         list_data.append(dict_object)
@@ -82,8 +69,8 @@ def get_save_data(retrieved_data):
             return processed
 
 
-def csv_extraction(retrieved_data):
-    header = create_header(retrieved_data)
+def csv_extraction(retrieved_data, search_data):
+    header = create_header(search_data)
     save_data = get_save_data(retrieved_data)
 
     with open('names.csv', 'w') as csv_file:
--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -1,6 +1,44 @@
 import unittest
 
+from simpleARS import extraction
+
 
 class TestExtraction(unittest.TestCase):
     def setUp(self):
-        self.extracted_data = {}
+        self.search = {
+            "data": [{"records": ["dataset_id", "category", {"amentities": ["name", "id"]},
+                                  {"weather": ["airport_ref", "wind_speed"]}]}]}
+
+        self.search_v2 = {"data": [{"records": [{"amentities": ["name", "id"]}]}]}
+
+        self.search_v3 = {"data": ["total", {"records": ["dataset_id"]}]}
+
+        self.search_v4 = {"~": [{"data": ["total", {"records": ["dataset_id"]}]}, "status_text", "status_code"]}
+
+        self.search_v5 = {"~": ["data", "status_text", "status_code"]}
+
+        self.search_v6 = {"~": ["id", "name", {"profile": ["name", "email"]}, {"facet": ["name", "dateCreated"]}]}
+
+    def test_check_if_create_header_function_returns_correct_header(self):
+        header = extraction.create_header(self.search)
+        self.assertEqual(header, ["dataset_id", "category", "name", "id", "airport_ref", "wind_speed"])
+
+    def test_check_if_create_header_function_returns_correct_header_v2(self):
+        header_v2 = extraction.create_header(self.search_v2)
+        self.assertEqual(header_v2, ["name", "id"])
+
+    def test_check_if_create_header_function_returns_correct_header_v3(self):
+        header_v3 = extraction.create_header(self.search_v3)
+        self.assertEqual(header_v3, ["total", "dataset_id"])
+
+    def test_check_if_create_header_function_returns_correct_header_v4(self):
+        header_v4 = extraction.create_header(self.search_v4)
+        self.assertEqual(header_v4, ["total", "dataset_id", "status_text", "status_code"])
+
+    def test_check_if_create_header_function_returns_correct_header_v5(self):
+        header_v5 = extraction.create_header(self.search_v5)
+        self.assertEqual(header_v5, ["data", "status_text", "status_code"])
+
+    def test_check_if_create_header_function_returns_correct_header_v6(self):
+        header_v6 = extraction.create_header(self.search_v6)
+        self.assertEqual(header_v6, ["id", "name", "name", "email", "name", "dateCreated"])
