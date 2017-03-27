798e193466413143ff86714657e36bce58ca1634
m19t12 <emmtsoukalas@gmail.com>
2017-03-27 17:37:33 +0300

    added comment to extraction.py file

--- simpleARS/extraction.py
+++ simpleARS/extraction.py
@@ -12,6 +12,11 @@ extraction functionalities
 
 
 def create_header(search_json):
+    """
+    method for creating header data for the csv file
+    :param search_json: search parameters in json format
+    :return: list of header data [header_1, header_2, header_3]
+    """
     header = []
     search = search_object.SearchObject(search_json)
     select_keys = search.src_select
@@ -28,6 +33,11 @@ def create_header(search_json):
 
 
 def find_relationships(save_data):
+    """
+    method for finding if data relationship is one to many [data_1, [list_data_2], data_2]
+    :param save_data: retrieved data from the filtering process
+    :return: return the data with the relationship
+    """
     processed_data = {}
     list_data = []
 
@@ -48,6 +58,11 @@ def find_relationships(save_data):
 
 
 def get_save_data(retrieved_data):
+    """
+    method for serializing the retrieved data and converted for csv extraction
+    :param retrieved_data: the data retrieved from the core retrieve process 
+    :return: return the data for saving in array containing dicts [{dict_1}, {dict_2}]
+    """
     headers = []
 
     for key in retrieved_data:
@@ -75,6 +90,13 @@ def get_save_data(retrieved_data):
 
 
 def csv_extraction(retrieved_data, search_data, csv_file_name):
+    """
+    the main extraction method for creating csv file
+    :param retrieved_data: the data retrieved from the core functionality
+    :param search_data: the search parameters in json format
+    :param csv_file_name: the name for the csv file you created
+    :return: the csv file
+    """
     header = create_header(search_data)
     save_data = get_save_data(retrieved_data)
 
