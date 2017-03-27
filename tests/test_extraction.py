648b880e785a0fb22333d3991142e308ea25dda3
m19t12 <emmtsoukalas@gmail.com>
2017-03-27 18:19:06 +0300

    add some tests in test_extraction.py

--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -19,6 +19,12 @@ class TestExtraction(unittest.TestCase):
 
         self.search_v6 = {"~": ["id", "name", {"profile": ["name", "email"]}, {"facet": ["name", "dateCreated"]}]}
 
+        self.retrieved_data = {"from_data_1": "data_1", "from_data_2": "data_2",
+                               "from_list_data_1": [{"from_sub_data_1": "data_1"}, {"from_sub_data_2": "data_2"}]}
+
+        self.retrieved_data_v2 = {
+            "from_data_1": [{"from_sub_data_1": [{"from_list_data_1": "data_1"}, {"from_list_data_2": "data_2"}]}]}
+
     def test_check_if_create_header_function_returns_correct_header(self):
         header = extraction.create_header(self.search)
         self.assertEqual(header, ["dataset_id", "category", "name", "id", "airport_ref", "wind_speed"])
@@ -42,3 +48,12 @@ class TestExtraction(unittest.TestCase):
     def test_check_if_create_header_function_returns_correct_header_v6(self):
         header_v6 = extraction.create_header(self.search_v6)
         self.assertEqual(header_v6, ["id", "name", "name", "email", "name", "dateCreated"])
+
+    def test_if_get_saved_data_return_correct_results_with_sub_data_relationship_one_to_many(self):
+        returned_data = extraction.get_save_data(self.retrieved_data)
+        self.assertEqual(returned_data,
+                         [{'from_data_1': 'data_1', 'from_data_2': 'data_2', 'from_sub_data_1': 'data_1'}])
+
+    def test_if_get_saved_data_return_correct_one_key_results(self):
+        returned_data = extraction.get_save_data(self.retrieved_data_v2)
+        self.assertEqual(returned_data, [{'from_list_data_1': 'data_1'}])
