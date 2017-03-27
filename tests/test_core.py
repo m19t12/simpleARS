c2707847bf41b4fa860cfe301b94fffe5905161a
m19t12 <emmtsoukalas@gmail.com>
2017-03-27 19:01:48 +0300

    add some tests to check if csv file export is correct test_extraction.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,6 @@
 import unittest
 
-from simpleARS import core
+from simpleARS import core, search_object
 
 
 class CoreTestCase(unittest.TestCase):
@@ -10,6 +10,8 @@ class CoreTestCase(unittest.TestCase):
                 {'dataset_id': 1000, 'amentities': [{'name': 'bar', 'id': 1}],
                  'weather': {'wind_speed': 'full'}}
             ]}, 'total': 300}
+        self.data = {"from_data_1": "data_1", "from_data_2": "data_2", "from_data_3": "data_3"}
+        self.search_root = {"~": ["from_data_1", "from_data_2", "from_data_3"]}
         self.search = {'data': [{'records': ['dataset_id', {'amentities': ['name', 'id']}]}]}
         self.retrieved_records = {'dataset_id': 1000, 'amentities': [{'name': 'bar', 'id': 1}],
                                   'weather': {'wind_speed': 'full'}}
@@ -32,3 +34,13 @@ class CoreTestCase(unittest.TestCase):
     def test_if_search_and_retrieve_return_top_data(self):
         results = core.search_and_retrieve(self.api_response['total'], [], 'total')
         self.assertEqual(results, {'total': 300})
+
+    def test_if_retrieve_sub_data_returns_correct_data_when_tilda_sign_used(self):
+        search_obj = search_object.SearchObject(self.search_root)
+        results = core.retrieve_sub_data(self.data, search_obj)
+        self.assertEqual(results, {"from_data_1": "data_1", "from_data_2": "data_2", "from_data_3": "data_3"})
+
+    def test_if_retrieve_sub_data_returns_none_when_no_key_exists(self):
+        search_obj = search_object.SearchObject({"data": ["data_1", "data_2"]})
+        results = core.retrieve_sub_data(self.data, search_obj)
+        self.assertEqual(results, None)
